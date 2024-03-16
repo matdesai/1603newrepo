9a80f4ad8510af203bf741134abc2aa2c9f631b1
matinDesai <matindesai7774@gmail.com>
2024-03-16 13:55:20 +0530

    this new commit new repo 1603

---
+++ src/pack1/m1.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class m1 {
+	
+	
+	public static void j1() {
+		
+		System.out.println("amhi kolhapuri");
+	}
+
+}
---
+++ src/pack1/m2.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class m2 extends m1{
+	
+	
+	public void j2() {
+		
+		System.out.println("hi am matin ");
+		
+	}
+
+}
